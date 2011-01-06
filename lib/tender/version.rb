0ef0c6b0353929702e7600ad0251e7f9236e5f73
Nash Kabbara <nash@zipzoomauto.com>
2011-01-06 17:21:05 -0600

    * cleanup

--- lib/tender/version.rb
+++
@@ -1,3 +0,0 @@
-module Tender
-  VERSION = "0.3"
-end
--- spec/tender_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), "./spec_helper")
-
-describe Tender, "#configure" do
-  it "should set the auth_token attribute" do
-    Tender.configure { |tender| tender.auth_token = "MyTestT" }
-    Tender.auth_token.should == "MyTestT"
-  end
-end
